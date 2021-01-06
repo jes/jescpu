6a23154a4c7099395d752923338663c7e99320f7
James Stanley <james@incoherency.co.uk>
2021-01-06 01:13:40 +0000

    Output once per loop

--- memcycle.s
+++ memcycle.s
@@ -1,11 +1,14 @@
 loop:
-copy adder+1 pointer
+copy adder+1 pointer # put the "pointer" address in adder argument
 adder: add 0 _1      # increase the value at the address
 add pointer _1       # move the pointer to the next address
 jz pointer resetloop # reset loop if pointer has wrapped
 jmp loop
+
 resetloop:
-copy pointer origpointer
+copy pointer origpointer # reset the pointer
+copy outputter+1 pointer # put the "pointer" address in outputter argument
+outputter: out 0 0       # output whatever "pointer" points to
 jmp loop
 
 origpointer: membase
