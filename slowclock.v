67a21cc4d7a042dd6f3d7ceec3fab8f2ed8fb2dd
James Stanley <james@incoherency.co.uk>
2021-01-08 09:46:39 +0000

    Make 'count' access clearer in clock

--- slowclock.v
+++ slowclock.v
@@ -4,10 +4,11 @@ module slowclock (clk_in, clk_out);
     reg [31:0] count;
 
     always @ (posedge clk_in) begin
-        count <= count + 1;
-        if (count >= 7) begin
+        if (count >= 8) begin
             count <= 0;
             clk_out <= ~clk_out;
+        end else begin
+            count <= count + 1;
         end
     end
 endmodule
